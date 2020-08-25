f76f376a9a815141b904902b9e08290084535936
OsvaldoZakowicz <osvaldozakowicz@gmail.com>
2020-08-25 17:18:06 -0300

    inicializar nivel, colores, generar secuencia de numeros de 0 a 3

--- script/main.js
+++ script/main.js
@@ -13,9 +13,29 @@ const btnEmpezar = document.getElementById('btnEmpezar')
 class Juego {
   constructor(){
     this.inicializar()
+    this.generarSecuencia()
   }
+  //inicio del juego
   inicializar(){
     btnEmpezar.classList.add('hide') //clase .hide en css main
+    this.nivel = 1
+    this.colores = {
+      celeste : celeste,
+      violeta : violeta,
+      naranja : naranja,
+      verde : verde
+    }
+  }
+  //generar valores aleatorios 0,1,2,3
+  generarSecuencia(){
+    /* Mapeo de secuencia de numeros al array:
+        Math.random() = valores decimales [0,1)
+        multiplicado por cuatro, valores decimales entre [0,3.99999)
+        Math.floor() = redondeo hacia abajo para tomar solo 0,1,2,3
+      */
+    this.secuencia = new Array(10).fill(0).map(
+      n => Math.floor(Math.random() * 4)
+    )
   }
 }
 
